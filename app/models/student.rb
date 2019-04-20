06dd54051a475ffc52ab614fdc133b3a80ed3c54
Jonathan Luk <jluk87@gmail.com>
2019-04-20 19:19:22 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def search(name)
+  def self.search(name)
     unless search_attributes.blank?
       Student.find_by(name: name)
     else
